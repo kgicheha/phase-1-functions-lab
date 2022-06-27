459b77d708df793db35bec8d1e59ee16b3a4f5cd
Kevin Gicheha <gichehakevin@gmail.com>
2022-06-27 14:33:58 -0700

    commit tree 74dee5d12cba4f05868615ffc8497167796cbf51

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const hq = 42
+function distanceFromHqInBlocks(destination) {
+    return Math.abs(destination- hq)
+}
+function distanceFromHqInFeet(destination) {
+    return (distanceFromHqInBlocks(destination) * 264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start -destination) *264
+}
+
+function calculatesFarePrice (start, destination){
+    let distance = Math.abs(start-destination)* 264
+    if (distance < 400) {
+        return 0;
+    }
+    else if(distance >= 400 && distance <= 2000) {
+        return parseFloat((distance * 0.02 -8).toFixed(2));
+    }
+    else if(distance > 2000 && distance <= 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far'
+    }
+}
